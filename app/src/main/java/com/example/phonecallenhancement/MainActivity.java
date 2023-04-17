93a18c24c194559996b9dca95e501c72d6b4e9eb
Alan Yeung <yeungbluecat123@gmail.com>
2023-04-17 01:13:16 -0700

    a

--- app/src/main/java/com/example/phonecallenhancement/MainActivity.java
+++ app/src/main/java/com/example/phonecallenhancement/MainActivity.java
@@ -3,21 +3,72 @@ package com.example.phonecallenhancement;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private static final int REQUEST_CODE = 0;
+    private DevicePolicyManager mDPM;
+    private ComponentName mAdminName;
+
+    public void onRequestPermission(View v) {
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                "android.permission.MANAGE_OWN_CALLS",
+                "android.permission.READ_PHONE_STATE",
+                "android.permission.PROCESS_INCOMING_CALLS",
+                "android.permission.PROCESS_OUTGOING_CALLS",
+                "android.permission.MANAGE_OWN_CALLS",
+                "android.permission.READ_PHONE_STATE",
+                "android.permission.PROCESS_INCOMING_CALLS",
+                "android.permission.PROCESS_OUTGOING_CALLS",
+                "android.permission.WRITE_EXTERNAL_STORAGE",
+                "android.permission.RECORD_AUDIO",
+                "android.permission.READ_CALL_LOG",
+                "android.permission.WRITE_EXTERNAL_STORAGE",
+                "android.permission.STORAGE"}, 1);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            // Initiate DevicePolicyManager.
+            mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+            mAdminName = new ComponentName(this, DeviceAdminDemo.class);
+
+            if (!mDPM.isAdminActive(mAdminName)) {
+                Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mAdminName);
+                intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Click on Activate button to secure your application.");
+                startActivityForResult(intent, REQUEST_CODE);
+            } else {
+                // mDPM.lockNow();
+                // Intent intent = new Intent(MainActivity.this,
+                // TrackDeviceService.class);
+                // startService(intent);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void onRequestPermission(View v)
-    {
-        ActivityCompat.requestPermissions(MainActivity.this, new String[] { "android.permission.MANAGE_OWN_CALLS", "android.permission.READ_PHONE_STATE", "android.permission.PROCESS_INCOMING_CALLS", "android.permission.PROCESS_OUTGOING_CALLS" }, 1);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (REQUEST_CODE == requestCode) {
+            Intent intent = new Intent(MainActivity.this, TService.class);
+            startService(intent);
+        }
     }
+
 }
--- app/src/main/java/com/example/phonecallenhancement/ReceivingCall.java
+++ app/src/main/java/com/example/phonecallenhancement/ReceivingCall.java
@@ -1,12 +1,20 @@
 package com.example.phonecallenhancement;
 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaRecorder;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
-public class ReceivingCall {
+import java.io.File;
+import java.io.IOException;
+
+public class ReceivingCall  extends BroadcastReceiver {
+    MediaRecorder callrecorder;
+
     public void onReceive(Context context, Intent intent) {
 
         Log.d("call", "call coming: ");
@@ -15,6 +23,29 @@ public class ReceivingCall {
         if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
             String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
             Toast.makeText(context, "Hey! Calling Number : " + incomingNumber, Toast.LENGTH_LONG).show();
+
+            String fname="rec"+System.currentTimeMillis()+".amr";
+            File file=new File(context.getFilesDir(),fname);
+
+            callrecorder=new MediaRecorder();
+            callrecorder.setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION);
+            callrecorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
+            callrecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            callrecorder.setOutputFile(file.getAbsolutePath());
+            try {
+                callrecorder.prepare();
+                callrecorder.start();
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }else if(state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+            if(callrecorder != null) {
+                callrecorder.stop();
+                callrecorder.reset();
+            }
+            Toast.makeText(context, "Hanged!", Toast.LENGTH_LONG).show();
         }
     }
 
